39225b123191f314c3598b60024590e73e793458
toni <ferreirocouto@gmail.com>
2020-04-16 19:42:21 +0200

    api rest

---
+++ src/main/java/com/toni/springboot/app/controllers/ClienteRestController.java
@@ -0,0 +1,22 @@
+package com.toni.springboot.app.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.toni.springboot.app.models.service.IClienteService;
+import com.toni.springboot.app.view.xml.ClienteList;
+
+@RestController
+@RequestMapping("/api/clientes")
+public class ClienteRestController {
+
+	@Autowired
+	private IClienteService clienteService;
+	
+	@GetMapping(value = "/listar")
+	public ClienteList listar() {
+		return new ClienteList(clienteService.findAll());
+	}
+}
