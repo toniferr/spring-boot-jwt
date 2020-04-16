525a0aec324a5c67a160997f6c5dc2fd11cae300
toni <ferreirocouto@gmail.com>
2020-04-16 19:15:06 +0200

    commit inicial

---
+++ src/main/java/com/toni/springboot/app/MvcConfig.java
@@ -0,0 +1,54 @@
+package com.toni.springboot.app;
+
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+	
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/error_403").setViewName("error_403");
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(new Locale("es", "ES"));
+		return localeResolver;
+	}
+	
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
+		localeInterceptor.setParamName("lang");
+		return localeInterceptor;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
+	}
+	
+	@Bean
+	public Jaxb2Marshaller jaxb2Marshaller() {
+		Jaxb2Marshaller marshaller =  new Jaxb2Marshaller();
+		marshaller.setClassesToBeBound(new Class[] {com.toni.springboot.app.view.xml.ClienteList.class});
+		return marshaller;
+	}
+}
---
+++ src/main/java/com/toni/springboot/app/models/service/IClienteService.java
@@ -0,0 +1,38 @@
+package com.toni.springboot.app.models.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.toni.springboot.app.models.entity.Cliente;
+import com.toni.springboot.app.models.entity.Factura;
+import com.toni.springboot.app.models.entity.Producto;
+
+public interface IClienteService {
+
+	public List<Cliente> findAll();
+	
+	public Page<Cliente> findAll(Pageable pageable);
+
+	public void save(Cliente cliente);
+	
+	public Cliente findOne(Long id);
+	
+	public Cliente fetchByIdWithFacturas(Long id);
+	
+	public void delete(Long id);
+	
+	public List<Producto> findByNombre(String term);
+	
+	public void saveFactura(Factura factura);
+	
+	public Producto findProductoById(Long id);
+	
+	public Factura findFacturaById(Long id);
+	
+	public void deleteFactura(Long id);
+	
+	public Factura fetchFacturaByIdWithClienteWhithItemFacturaWithProducto(Long id);
+
+}
